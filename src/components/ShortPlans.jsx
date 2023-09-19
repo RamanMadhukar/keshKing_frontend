4a4da62f35cbf26ee76f4fe43c3c5075197287af
Raman Madhukar <71077580+RamanMadhukar@users.noreply.github.com>
2023-09-20 01:35:58 +0530

    successfull purches align

--- src/components/ShortPlans.jsx
+++ src/components/ShortPlans.jsx
@@ -152,11 +152,11 @@ const ShortPlans = () => {
                     setBalanceIndicator(false);
                 }, 3000);
             }
-            else if(userDetails.plans_purchased.filter(e=>e.plan_name==='Kesh King 1').length>0 && currPlan.plan_name==='Kesh King 1') {
+            else if (userDetails.plans_purchased.filter(e => e.plan_name === 'Kesh King 1').length > 0 && currPlan.plan_name === 'Kesh King 1') {
                 setIsOpen(false);
                 toaster('You can only purchase this plan once');
                 return;
-            } 
+            }
             else {
                 await axios.post(`${BASE_URL}/purchase`, {
                     recharge_amount: Number(userDetails.recharge_amount) - Number(Number(quantity) * Number(currPlan.plan_amount)),
@@ -224,16 +224,19 @@ const ShortPlans = () => {
                 </div>
             </div> : null}
 
-            {planPurchaseShow ? <div className='absolute w-[40%]  top-[450px] rounded-lg shadow-xl  z-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-                <div className='flex flex-col justify-center gap-3 h-[180px] shadow-2xl border border-gray-300 items-center bg-white w-full text-cyan-400 rounded-xl'>
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 fill-cyan-400">
-                            <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
-                        </svg>
+            {planPurchaseShow ?
+                <div className='absolute w-[40%]  top-[450px] rounded-lg shadow-xl  z-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+                    <div className='flex flex-col justify-center gap-3 h-[180px] shadow-2xl border border-gray-300 items-center bg-white w-full text-cyan-400 rounded-xl'>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 fill-cyan-400">
+                                <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+                            </svg>
+                        </div>
+                        <div className='text-sm text-center font-extrabold'>Successful Purchase</div>
                     </div>
-                    <div className='text-lg font-extrabold'>Successful Purchase</div>
                 </div>
-            </div> : null}
+                :
+                null}
 
             <div >
                 <ReactModal
